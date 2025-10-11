a8d435dc147d9ec3dfdaf94e8d90ddeaca1bdc9c
heisenburger <51046987+heisenburgah@users.noreply.github.com>
2025-10-12 03:18:37 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1035,7 +1035,7 @@ end
 
 local FetchIcons, Icons = pcall(function()
     return loadstring(
-        game:HttpGet("https://raw.githubusercontent.com/deividcomsono/lucide-roblox-direct/refs/heads/main/source.lua")
+		game:HttpGet("https://raw.githubusercontent.com/heisenburgah/HYDROXIDE/refs/heads/main/Source.lua")
     )()
 end)
 function Library:GetIcon(IconName: string)
